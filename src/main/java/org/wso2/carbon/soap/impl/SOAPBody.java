7bf3a0c4e2de6139a91aeaefb99fa2f436120ca1
Natashawso2 <natasha@wso2.com>
2016-05-23 08:17:10 +0530

    Commiting local changes

--- src/main/java/org/wso2/carbon/soap/impl/SOAPBody.java
+++ src/main/java/org/wso2/carbon/soap/impl/SOAPBody.java
@@ -1,17 +1,17 @@
 /**
- *  Copyright (c) 2015 WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Copyright (c) 2015 WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.wso2.carbon.soap.impl;
 
@@ -105,9 +105,10 @@ public class SOAPBody extends ElementImpl {
             str = stw.toString();
 
         } catch (TransformerConfigurationException e) {
-            e.printStackTrace();
+            new SOAPException("Configuration error when converting the element to string");
         } catch (TransformerException e) {
-            e.printStackTrace();
+            new SOAPException("Exceptional condition that occured during the transformation process" +
+                    " when converting the element to string");
         }
 
 
